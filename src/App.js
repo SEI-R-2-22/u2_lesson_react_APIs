c2a46e8f078e78b46c5d68c312768a2301ed1c5f
Michael Lackey <michael.g.lackey@gmail.com>
2022-04-18 19:18:52 -0500

    update to React 18

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 
 const App = () => {
   const [movies, setMovies] = useState([])
-  const [selectedMovie, setSelectedMovie] = useState(null) // this should be a movie once you select one
+  const [selectedMovie, setSelectedMovie] = useState(null)
+  // this should be a single movie object once you've select one
 
   return (
     <div>
